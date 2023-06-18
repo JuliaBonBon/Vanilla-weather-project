97848398bbb7307a7d60d08ae9a2486c561e2be7
JuliaBonBon <juliabonbon1@gmail.com>
2023-06-18 19:48:32 +0100

    Added background to HTML, CSS

--- src/index.js
+++ src/index.js
@@ -71,6 +71,7 @@ function displayFahrenheitTemperature(event) {
 
 function displayCelsiusTemperature(event) {
   event.preventDefault();
+
   celsiusLink.classList.add("active");
   fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
