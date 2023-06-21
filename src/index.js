3ae234a03bafee660683c62371ebc88184755163
JuliaBonBon <juliabonbon1@gmail.com>
2023-06-21 21:48:25 +0100

    injected Div "forecast" with new function

--- src/index.js
+++ src/index.js
@@ -21,6 +21,41 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+            <div class="col-2">
+              <div class="weather-forecast-data">
+               ${day}
+              </div>
+              <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="" width="42" />
+              <div>
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperatures-max">18°</span>
+                  <span class="weather-forecast-temperatures-min">12°</span>
+                </div>
+              </div>
+            </div>
+          
+          `;
+  });
+  forecast = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "0efb4fc16a9ed98dc0b3aafd8491d6ad";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -44,6 +79,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   icon.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
